2a857f2cbdad13ed01fa6e8555a058481562d2f6
mattthrows <81485154+mattthrows@users.noreply.github.com>
2023-02-18 11:30:25 -0800

    fix linting nightmare

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  extends: ["./node_modules/@spm/eslint-config/index.js"],
+  overrides: [
+    {
+      files: ['src/**/*.js'],
+      rules: {
+        // 0 is off / 1 is warn / 2 is error
+        'react-hooks/exhaustive-deps': 2,       
+      },
+    },
+  ],
 };
